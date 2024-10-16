edc83c2150b60df6a8e1b741aa38a037e523e67c
Szymon Slawinski <sslawins@pcwaw09.dyndns.cern.ch>
2024-10-16 18:42:04 +0200

    added some basic histograms

--- UserCode/Analysis/test/Analysis.cc
+++ UserCode/Analysis/test/Analysis.cc
@@ -51,7 +51,17 @@ private:
   edm::ParameterSet theConfig;
   unsigned int theEventCount;
 
-  TH1D *pT;
+  TH1D *BspT;
+  TH1D *BpT;
+  TH1D *BToBs;
+
+  TH1D *mupT;
+  TH1D *gammapT;
+
+  TH1D *muEta;
+  TH1D *gammaEta;
+
+  TH2D *mu1Eta_mu2Eta;
 
   edm::EDGetTokenT < vector<reco::GenParticle> > theGenParticleToken;
 
@@ -76,8 +86,17 @@ Analysis::~Analysis()
 void Analysis::beginJob()
 {
   //create a histogram
-  pT = new TH1D("pT","pT; pT [GeV]; #events",100, 0., 50.);
+  BspT = new TH1D("BspT","Bs pT; pT [GeV]; #events",100, 0., 50.);
+  BpT = new TH1D("BpT","B (not Bs); pT [GeV]; #events",100, 0., 50.);
+  BToBs = new TH1D("BsToB","Bs to B; pT [GeV]; #events",100, -1, 4);
 
+  mupT = new TH1D("mupT","mu pT; pT [GeV]; #events",100, 0., 50.);
+  gammapT = new TH1D("gammapT","gamma pT; pT [GeV]; #events",100, 0., 50.);
+
+  muEta = new TH1D("muEta","mu eta; eta; #events",100, -4., 4.);
+  gammaEta = new TH1D("gammaEta","gamma eta; eta; #events",100, -4., 4.);
+
+  mu1Eta_mu2Eta = new TH2D("mu1Eta_mu2Eta","mu1 eta vs mu2 eta; mu1 eta; mu2 eta; #events",100, -4., 4., 100, -4., 4.);
 
   cout << "HERE Analysis::beginJob()" << endl;
 }
@@ -88,11 +107,19 @@ void Analysis::endJob()
   TFile myRootFile( theConfig.getParameter<std::string>("outHist").c_str(), "RECREATE");
 
   //write histogram data
-  pT -> Write();
+  BspT -> Write();
+  BpT -> Write();
+  BToBs -> Write();
+
+  mupT -> Write();
+  gammapT -> Write();
+
+  muEta -> Write();
+  gammaEta -> Write();
+
+  mu1Eta_mu2Eta -> Write();
 
   myRootFile.Close();
-
-  delete pT;
 
 
   cout << "HERE Analysis::endJob()" << endl;
@@ -106,6 +133,8 @@ void Analysis::analyze(
   //bool debug = true;
 
   const vector<reco::GenParticle> & genParv = ev.get(theGenParticleToken);
+  int nBs  = 0;
+  int nB   = 0;
 
   for(auto genPar : genParv)
   {
@@ -115,19 +144,52 @@ void Analysis::analyze(
       cout << "\t daughter PDGid:";
 
       bool isNotFinal = false;
+
+      vector<float> muEtaV;
+      muEtaV.clear();
       for(unsigned int i=0; i < genPar.numberOfDaughters(); i++)
       {
         int d_pdgid = genPar.daughterRef(i)->pdgId();
         cout << " " << d_pdgid;
         if (abs(d_pdgid)/100 == 5) isNotFinal = true;
 
+        if (abs(genPar.pdgId() == 531) && abs(d_pdgid) == 13)
+        {
+          mupT->Fill(genPar.daughterRef(i)->pt());
+          muEta->Fill(genPar.daughterRef(i)->eta());
+          muEtaV.push_back(genPar.daughterRef(i)->eta());
+        }
+
+        if (abs(genPar.pdgId() == 531) && abs(d_pdgid) == 22)
+        {
+          gammapT->Fill(genPar.daughterRef(i)->pt());
+          gammaEta->Fill(genPar.daughterRef(i)->eta());
+        }
+
       }
       cout << endl << endl;
 
-      if (abs(genPar.pdgId()) == 531 && !isNotFinal) pT->Fill(genPar.pt());
+      if (muEtaV.size() == 2)
+      {
+        mu1Eta_mu2Eta->Fill(muEtaV[0], muEtaV[1]);
+      }
+
+      if (abs(genPar.pdgId()) == 531 && !isNotFinal)
+      {
+        BspT->Fill(genPar.pt());
+        nBs++;
+      }
+      if (abs(genPar.pdgId()) == 511 && !isNotFinal)
+      {
+        BpT->Fill(genPar.pt());
+        nB++;
+      }
     }
 
+
   } // particle loop
+
+  BToBs->Fill((float)nB/nBs);
   cout <<"*** Analyzed event: " << ev.id()<<" analysed event count:"<<++theEventCount << endl << endl;
 }
 
--- UserCode/Analysis/test/plot.py
+++ UserCode/Analysis/test/plot.py
@@ -1,13 +1,17 @@
 import ROOT
 import sys
+from ROOT import gROOT
+
+gROOT.SetBatch(True)
 
 filename = sys.argv[1]
 file = ROOT.TFile(filename)
 
+c = ROOT.TCanvas()
+
 for key in file.GetListOfKeys():
     hist = file.Get(key.GetName())
-
-    c = ROOT.TCanvas()
+    c.Clear()
     hist.Draw()
     c.SaveAs('Plots/' + key.GetName() + '.png')
 
