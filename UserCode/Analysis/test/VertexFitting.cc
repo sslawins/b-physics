9af25a191ce0f6d560cd3dcf01af1d982b095e3f
Szymon <szymon.slawinski02@gmail.com>
2024-12-03 10:20:27 +0100

    added muon vertex fitting

---
+++ UserCode/Analysis/test/VertexFitting.cc
@@ -0,0 +1,258 @@
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/PackedCandidate.h"
+
+#include "DataFormats/Candidate/interface/Candidate.h"
+
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/EgammaCandidates/interface/Photon.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "FWCore/Common/interface/TriggerNames.h"
+
+#include "DataFormats/Math/interface/deltaR.h"
+
+#include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
+#include "TrackingTools/Records/interface/TransientTrackRecord.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+#include "RecoVertex/VertexPrimitives/interface/TransientVertex.h"
+#include "RecoVertex/KalmanVertexFit/interface/KalmanVertexFitter.h"
+
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Candidate/interface/VertexCompositePtrCandidate.h"
+
+
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TFile.h"
+#include "TMath.h"
+#include "TLorentzVector.h"
+
+#include <sstream>
+#include <iomanip> 
+#include <utility>
+#include <numeric>
+
+
+using namespace std;
+
+
+//object definition
+class VertexFitting : public edm::one::EDAnalyzer<> {
+public:
+
+  //constructor, function is called when new object is created
+  explicit VertexFitting(const edm::ParameterSet& conf);
+
+  //destructor, function is called when object is destroyed
+  ~VertexFitting();
+
+  //edm filter plugin specific functions
+  virtual void beginJob();
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  virtual void endJob();
+
+  bool isSameDecay(const std::vector<int>&, const std::vector<int>&);
+private:
+
+  edm::ParameterSet theConfig;
+  unsigned int theEventCount;
+
+  edm::EDGetTokenT < vector<reco::GenParticle> > theGenParticleToken;
+  edm::EDGetTokenT < vector<reco::Muon> > theMuonToken;
+  edm::EDGetTokenT < vector<reco::Photon> > thePhotonToken;
+
+  edm::ESGetToken<TransientTrackBuilder, TransientTrackRecord> theTrackBuilderToken;
+
+  // histograms
+  TH1D* hFittedVsGenVertexDistance;
+  TH1D* hDistanceToPV;
+
+  std::vector<int> MuMuG = {22, 13, -13};
+
+};
+
+
+VertexFitting::VertexFitting(const edm::ParameterSet& conf)
+  : theConfig(conf), theEventCount(0)
+{
+  cout <<" CTORXX" << endl;
+
+  theGenParticleToken = consumes< vector<reco::GenParticle>  >( edm::InputTag("genParticles"));
+  theMuonToken = consumes< vector<reco::Muon>  >( edm::InputTag("muons"));
+  thePhotonToken = consumes< vector<reco::Photon>  >( edm::InputTag("photons"));
+
+  theTrackBuilderToken = esConsumes(edm::ESInputTag("", "TransientTrackBuilder"));
+}
+
+VertexFitting::~VertexFitting()
+{
+  cout <<" DTOR" << endl;
+}
+
+bool VertexFitting::isSameDecay(const std::vector<int>& dec1, const std::vector<int>& dec2) {
+    
+    if (dec1.size() != dec2.size()) {
+        return false; 
+    }
+
+    std::set<int> dec1Set(dec1.begin(), dec1.end());
+    std::set<int> dec2Set(dec2.begin(), dec2.end());
+
+    return dec1Set == dec2Set;
+}
+
+
+void VertexFitting::beginJob()
+{
+  //create a histogram
+  hFittedVsGenVertexDistance = new TH1D("hFittedVsGenVertexDistance", "hFittedVsGenVertexDistance", 100, 0, 1);
+  hDistanceToPV = new TH1D("hDistanceToPV", "hDistanceToPV", 100, 0, 10);
+
+  cout << "HERE VertexFitting::beginJob()" << endl;
+}
+
+void VertexFitting::endJob()
+{
+  //make a new Root file
+  TFile myRootFile( theConfig.getParameter<std::string>("outHist").c_str(), "RECREATE");
+
+  //write histogram data
+  hFittedVsGenVertexDistance->Write();
+  hDistanceToPV->Write();
+
+  myRootFile.Close();
+
+  delete hFittedVsGenVertexDistance;
+  delete hDistanceToPV;
+
+  cout << "HERE VertexFitting::endJob()" << endl;
+}
+
+
+void VertexFitting::analyze(
+    const edm::Event& ev, const edm::EventSetup& es)
+{
+  std::cout << " -------------------------------- HERE VertexFitting::analyze "<< std::endl;
+
+  const std::vector<reco::GenParticle> & genPar = ev.get(theGenParticleToken);
+  const std::vector<reco::Muon> & recoMuons = ev.get(theMuonToken);
+  const std::vector<reco::Photon> & recoPhotons = ev.get(thePhotonToken);
+
+
+  vector<const reco::Candidate*> genMuons;
+  vector<const reco::Muon*> recoMatchedMuons;
+  vector<const reco::Candidate*> genMatchedMuons;
+
+  vector<const reco::Candidate*> genPhotons;
+  vector<const reco::Photon*> recoMatchedPhotons;
+  vector<const reco::Candidate*> genMatchedPhotons;
+
+  const auto & trackBuilder = es.getData(theTrackBuilderToken);
+
+  reco::Candidate::Point genBsPoint;
+
+  for(const auto& genP : genPar)
+  {
+    if (abs(genP.pdgId()) == 531)
+    {
+      vector<int> daughters;
+      for(unsigned int i=0; i < genP.numberOfDaughters(); i++)
+      {
+        daughters.push_back(genP.daughter(i)->pdgId());
+      }
+      if(isSameDecay(daughters, MuMuG))
+      {
+        genBsPoint = genP.vertex();
+        for(unsigned int i=0; i < genP.numberOfDaughters(); i++)
+        {
+          if(abs(genP.daughter(i)->pdgId()) == 13) genMuons.push_back(genP.daughter(i));
+          if(abs(genP.daughter(i)->pdgId()) == 22) genPhotons.push_back(genP.daughter(i));
+        }
+      }
+    }
+  }
+
+  // reco muon matching
+  for (const reco::Candidate* genMu : genMuons)
+  {
+    float minDR = 10;
+    const reco::Muon* bestMatchedMuon;
+    bool matched = false;
+    for (const auto& recoMu : recoMuons)
+    {
+      float dR = reco::deltaR(recoMu, *genMu);
+      if (dR < minDR)
+      {
+        minDR = dR;
+        bestMatchedMuon = &recoMu;
+        matched = true;
+      }
+    }
+    if (matched && minDR < 0.01)
+    {
+      recoMatchedMuons.push_back(bestMatchedMuon);
+      genMatchedMuons.push_back(genMu);
+    }
+  }
+
+  // reco photon matching
+  for (const reco::Candidate* genPh : genPhotons)
+  {
+    float minDR = 10;
+    const reco::Photon* bestMatchedPhoton;
+    bool matched = false;
+    for (const auto& recoPh : recoPhotons)
+    {
+      float dR = reco::deltaR(recoPh, *genPh);
+      if (dR < minDR)
+      {
+        minDR = dR;
+        bestMatchedPhoton = &recoPh;
+        matched = true;
+      }
+    }
+    if (matched && minDR < 0.03)
+    {
+      recoMatchedPhotons.push_back(bestMatchedPhoton);
+      genMatchedPhotons.push_back(genPh);
+    }
+  }
+  // vertex fitting
+
+  vector<reco::TransientTrack> muonTTs;
+  for (const auto& mu : recoMatchedMuons)
+  {
+    reco::TrackRef muTrack = mu->track();
+    if(!muTrack) continue;
+    muonTTs.push_back(trackBuilder.build(muTrack));
+  }
+  
+  if(muonTTs.size() == 2)
+  {
+    KalmanVertexFitter kvf(true);
+
+    reco::Vertex muonVertex = TransientVertex(kvf.vertex(muonTTs));
+
+    auto fittedPoint = muonVertex.position();
+
+    cout << "genBsPoint: " << genBsPoint << " fittedPoint: " << fittedPoint << endl;
+    hFittedVsGenVertexDistance->Fill((fittedPoint - genBsPoint).R());
+    hDistanceToPV->Fill(muonVertex.position().R());
+  }
+
+  cout <<"*** Analyze event: " << ev.id() <<" analysed event count:" << ++theEventCount << endl;
+}
+
+DEFINE_FWK_MODULE(VertexFitting);
+
